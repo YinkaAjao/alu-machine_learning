d99fdde27243bdee1aee70c283b4cc21381856d8
YinkaAjao <160027843+YinkaAjao@users.noreply.github.com>
2025-09-12 17:36:02 +0200

    2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-This module defines a function that calculates the shape of a matrix.
-"""
+#This module defines a function that calculates the shape of a matrix.
 
 def matrix_shape(matrix):
     shape = []
