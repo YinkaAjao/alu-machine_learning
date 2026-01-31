c02484cf6561490c193cd02b5f63f4d22764933d
YinkaAjao <160027843+YinkaAjao@users.noreply.github.com>
2026-01-31 14:44:34 +0200

    hyperparameter_tuning

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -63,31 +63,31 @@ class BayesianOptimization:
         X_next = self.X_s[np.argmax(EI)]
 
         return X_next, np.array(EI)
-
+    
     def optimize(self, iterations=100):
         """
         Optimizes the black-box function
         Args:
-            iterations: maximum number of iterations to perform
-
+        iterations: maximum number of iterations to perfor
+        
         Returns: X_opt, Y_opt
         """
-        for i in range(iterations):
+        for _ in range(iterations):
             X_next, _ = self.acquisition()
 
-            # Check if X_next is already in self.gp.X
-            # Use np.any with np.isclose for proper comparison
-            if np.any(np.isclose(self.gp.X, X_next[0])):
+            # Stop early if X_next already sampled
+            if np.any(np.isclose(self.gp.X, X_next).all(axis=1)):
                 break
 
-            Y = self.f(X_next)
-            self.gp.update(X_next, Y)
-
+            Y_next = self.f(X_next)
+            self.gp.update(X_next, Y_next)
+            
+        # Select optimal point
         if self.minimize:
             idx = np.argmin(self.gp.Y)
         else:
             idx = np.argmax(self.gp.Y)
-
         X_opt = self.gp.X[idx]
         Y_opt = np.array(self.gp.Y[idx])
+
         return X_opt, Y_opt
